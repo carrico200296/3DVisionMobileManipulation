c0cfd25c090f71ff773602cbeaa7bfde5a296cd1
carrico200296 <s192340@student.dtu.dk>
2021-03-24 16:02:57 +0100

    tf tree added

--- catkin_ws/build/pose_estimation_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ccdn/git/3DVisionMobileManipulation/catkin_ws/src/pose_estimation_pkg
-# Build directory: /home/ccdn/git/3DVisionMobileManipulation/catkin_ws/build/pose_estimation_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
